94790f971256e97edf109d14177563a704bb49eb
Luchito Paz <lpazd2006unp@gmail.com>
2018-11-16 11:22:39 -0500

    Initial Commit

---
+++ 01-iamrich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  01-iamrich
+//
+//  Created by everis on 16/11/18.
+//  Copyright © 2018 PazMestanza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
